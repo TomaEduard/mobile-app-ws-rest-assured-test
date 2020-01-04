7be4dc046d0b26d242b4f5f87710040622699417
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-01-04 23:19:47 +0200

    added test for update and delete user

--- src/test/java/com/app/mobileappwsrestassuretest/UsersWebServiceEndpointTest.java
+++ src/test/java/com/app/mobileappwsrestassuretest/UsersWebServiceEndpointTest.java
@@ -3,6 +3,7 @@ package com.app.mobileappwsrestassuretest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runners.MethodSorters;
@@ -24,6 +25,7 @@ public class UsersWebServiceEndpointTest {
 
     private static String authorizationHeader;
     private static String userId;
+    private static List<Map<String, String>> addresses;
 
     @BeforeEach
     void setUp() {
@@ -32,9 +34,9 @@ public class UsersWebServiceEndpointTest {
     }
 
     /*
-    * testUserLogin
-    *
-    */
+     * test UserLogin
+     *
+     */
     @Test
     final void a() {
 
@@ -59,7 +61,7 @@ public class UsersWebServiceEndpointTest {
     }
 
     /*
-     * testGetUserDetails()
+     * test Get UserDetails()
      *
      */
     @Test
@@ -81,7 +83,7 @@ public class UsersWebServiceEndpointTest {
         String lastName = response.jsonPath().getString("lastName");
         String userEmail = response.jsonPath().getString("email");
 
-        List<Map<String, String>> addresses = response.jsonPath().getList("addresses");
+        addresses = response.jsonPath().getList("addresses");
         String addressId1 = addresses.get(0).get("addressId");
         String addressId2 = addresses.get(1).get("addressId");
 
@@ -94,6 +96,73 @@ public class UsersWebServiceEndpointTest {
         assertEquals(2, addresses.size());
         assertEquals(30, addressId1.length());
         assertEquals(30, addressId2.length());
+    }
+
+    /*
+     * test update UserDetail()
+     *
+     */
+    @Test
+    final void c() {
+
+        Map<String, Object> userDetails = new HashMap<>();
+        userDetails.put("firstName", "Edd");
+        userDetails.put("lastName", "Tom");
+        userDetails.put("email", "edd@test.com");
+        userDetails.put("password", "aaa");
+
+        Response response = given()
+                .contentType(JSON)
+                .accept(JSON)
+                .header("Authorization",authorizationHeader)
+                .pathParam("id", userId)
+                .body(userDetails)
+                .when()
+                .put(CONTEXT_PATH + "/users/{id}")
+                .then()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract()
+                .response();
+
+        String firstName = response.jsonPath().getString("firstName");
+        String lastName = response.jsonPath().getString("lastName");
+        String email = response.jsonPath().getString("email");
+
+        List<Map<String, String>> storedAddresses = response.jsonPath().getList("addresses");
+
+        assertEquals("Edd", firstName);
+        assertEquals("Tom", lastName);
+        assertEquals("edd@test.com", email);
+        assertNotNull(storedAddresses);
+        assertEquals(addresses.size(), storedAddresses.size());
+        assertEquals(addresses.get(0).get("streetName"), storedAddresses.get(0).get("streetName"));
+
+    }
+
+    /*
+     * test delete UserDetail()
+     *
+     */
+    @Ignore
+    @Test
+    final void d() {
+
+        Response response = given()
+                .contentType(JSON)
+                .accept(JSON)
+                .header("Authorization",authorizationHeader)
+                .pathParam("id", userId)
+                .when()
+                .delete(CONTEXT_PATH + "/users/{id}")
+                .then()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract()
+                .response();
+
+        String operationResult = response.jsonPath().getString("operationResult");
+        assertEquals("SUCCESS", operationResult);
     }
 
 }
